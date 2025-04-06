1eda659b12211b7e2b92e786fddf706f10edc17c
Abdualsslam <abdualsslam222@gmail.com>
2025-04-07 01:59:45 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/yemenhistory/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yemenhistory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const YemenHistory());
+}
+
+class YemenHistory extends StatelessWidget {
+  const YemenHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
