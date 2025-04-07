d8a3ce9b9586cd5b99607dc9aef0f16f18467521
Abdualsslam <abdualsslam222@gmail.com>
2025-04-07 04:28:25 +0300

    finish Splash screen

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:yemenhistory/featuers/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:yemenhistory/featuers/splash/presentation/views/splash_view.dart';
 
 final GoRouter router = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const SplashView()),
+    GoRoute(path: '/', builder: (context, state) => SplashView()),
+    GoRoute(path: '/onBoarding', builder: (context, state) => const OnBoardingView()),
     // GoRoute(path: 'home', builder: (context, state) => const HomeView()),
   ],
 );
--- lib/featuers/splash/presentation/views/splash_view.dart
+++ lib/featuers/splash/presentation/views/splash_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:yemenhistory/core/navigation/navigation.dart';
+import 'package:yemenhistory/core/utils/app_strings.dart';
+import 'package:yemenhistory/core/utils/app_text_styles.dart';
 
-class SplashView extends StatelessWidget {
-  const SplashView({super.key});
+class SplashView extends StatefulWidget {
+  SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    delayedNavagte(context);
+    super.initState();
+  }
+
+  void delayedNavagte(context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      customNavigate(context, '/onBoarding');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(body: Center(child: Text(AppStrings.SPLASH_SCREEN, style: CustomTextStyles.pacifico400style42)));
   }
 }
+// This is a placeholder for the splash screen view. You can customize it as needed.
+// You can add a logo, animation, or any other content you want to display during the splash screen. 
