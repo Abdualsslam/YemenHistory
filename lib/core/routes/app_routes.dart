f5ea025585cda6357114c6287894f89898f717fa
Abdualsslam <abdualsslam222@gmail.com>
2025-04-09 02:27:41 +0300

    Refactor authentication views and integrate AuthCubit;  and set up service locator for AuthCubit

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
-import 'package:yemenhistory/featuers/auth/presentation/views/sign_in.dart';
-import 'package:yemenhistory/featuers/auth/presentation/views/sign_up.dart';
+import 'package:yemenhistory/featuers/auth/presentation/auth_cubit/cubit/auth_cubit.dart';
+import 'package:yemenhistory/featuers/auth/presentation/views/sign_in_view.dart';
+import 'package:yemenhistory/featuers/auth/presentation/views/sign_up_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yemenhistory/featuers/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:yemenhistory/featuers/splash/presentation/views/splash_view.dart';
 
@@ -8,7 +10,7 @@ final GoRouter router = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashView()),
     GoRoute(path: '/onBoarding', builder: (context, state) => const OnBoardingView()),
-    GoRoute(path: '/signUp', builder: (context, state) => const SignUpView()),
-    GoRoute(path: '/signIn', builder: (context, state) => const SignInView()),
+    GoRoute(path: "/signUp", builder: (context, state) => BlocProvider(create: (context) => AuthCubit(), child: const SignUpView())),
+    GoRoute(path: "/signIn", builder: (context, state) => BlocProvider(create: (context) => AuthCubit(), child: const SignInView())),
   ],
 );
--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,8 +1,9 @@
 import 'package:get_it/get_it.dart';
 import 'package:yemenhistory/core/database/cache/cache_helper.dart';
+import 'package:yemenhistory/featuers/auth/presentation/auth_cubit/cubit/auth_cubit.dart';
 
 final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<CacheHelper>(CacheHelper());
-  // getIt.registerSingleton<AuthCubit>(AuthCubit());
+  getIt.registerSingleton<AuthCubit>(AuthCubit());
 }
